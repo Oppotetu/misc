5e9b5943932aebb10444dc2529e588c564181998
simonhaughom <simon.haughom@gmail.com>
2022-12-09 22:24:10 +0100

    works but inefficient

--- Project_Euler/divisibleTriNum.js
+++ Project_Euler/divisibleTriNum.js
@@ -1,33 +1,45 @@
 function findTriangleNumbers(num) {
-  var triangleNumbers = [];
+  return (num * (num + 1)) / 2;
+}
+
+// function factorTriangleNumbers() {
+//   var factors = [];
+//   for (const i of findTriangleNumbers(20)) {
+//     for (j = 1; j <= i; j++) {
+//       if (i % j == 0) factors.push(j);
+//     }
+//     // console.log(i, factors);
+//     if (factors.length > 500) return i;
+//     factors = [];
+//   }
+
+//   var fail = "fail";
+//   return fail;
+// }
+
+// console.log(factorTriangleNumbers())
+
+function findMaxFactors(maxFactors) {
+  var factors = [];
+  var triNum = 0;
   var addCounter = 1;
 
-  for (let i = 1; i <= num; i++) {
-    var triNum = 0;
-    while (addCounter <= i) {
-      triNum += addCounter;
-      addCounter += 1;
+  
+  while (factors.length <= maxFactors) {
+    triNum += addCounter;
+    addCounter += 1;
+    for (i = 1; i <= triNum; i++) {
+      if (triNum % i == 0) factors.push(i);
     }
-    triangleNumbers.push(triNum);
-    addCounter = 0;
-  }
-  return triangleNumbers;
-}
-
-function factorTriangleNumbers() {
-  var factors = [];
-  for (const i of findTriangleNumbers(5000)) {
-    for (j = 1; j <= i; j++) {
-      if (i % j == 0) factors.push(j);
+    if (factors.length >= maxFactors) {
+      var result = "The triangle number " + triNum + " has " + factors.length + " factors: " + "\n" + factors.join(" ");
+      return result; 
     }
-    // console.log(i, factors);
-    if (factors.length > 500) return i;
     factors = [];
   }
-
-  var fail = "fail";
-  return fail;
 }
 
-console.log(factorTriangleNumbers())
-
+console.log(findMaxFactors(5));
+console.log(findMaxFactors(7));
+console.log(findMaxFactors(10));
+console.log(findMaxFactors(200));

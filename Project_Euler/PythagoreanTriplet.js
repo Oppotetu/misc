c2739db400aeb795e5ce6a9bba805d89274b350c
simonhaughom <simon.haughom@gmail.com>
2022-12-11 17:05:27 +0100

    pythagorean triplet done

--- Project_Euler/PythagoreanTriplet.js
+++ Project_Euler/PythagoreanTriplet.js
@@ -1,5 +1,23 @@
 
-
-function pythagoreanTriplet() {
-  var a = new Number
+function pythagoreanTriplet(sum) {
+  var a;
+  for (a = 1; a <= 400; a++) {
+    var b;
+    for (b = a + 1; b <= a + 200; b++) {
+      let apow = Math.pow(a, 2);
+      let bpow = Math.pow(b, 2);
+      let c = apow + bpow;
+      let croot = Math.sqrt(c);
+      if (!Number.isInteger(croot)) continue
+      if (a + b + croot == sum) {
+        let summ = a + b + croot;
+        let product = a * b * croot;
+        var res = "a = " + a + " b = " + b + " c = " + croot + "\n" + "sum: " + summ + "\n" + "product = " + product;
+        return res;
+      };
+    }
+  }
+  return "not found";
 }
+
+console.log(pythagoreanTriplet(1000));
